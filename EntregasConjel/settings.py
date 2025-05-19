f80149b795014237787cf06c1259a45400d9da54
Renan Melo <renanlaertemelo@gmail.com.br>
2025-05-19 11:03:17 -0300

    ajustes

--- EntregasConjel/settings.py
+++ EntregasConjel/settings.py
@@ -92,8 +92,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
